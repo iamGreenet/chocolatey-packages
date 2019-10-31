8a10e9915aff94f52fd58dc0b68e492670eda8a4
Greenet <lt.tt@outlook.com>
2019-10-31 15:47:16 +0800

    traffic-monitor: update to version 1.77

---
+++ traffic-monitor/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop';
+#$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/zhongyang219/TrafficMonitor/releases/download/V1.77/TrafficMonitor_V1.77_x86.7z'
+$url64 = 'https://github.com/zhongyang219/TrafficMonitor/releases/download/V1.77/TrafficMonitor_V1.77_x64.7z'
+$checksum = '3973D2DB7B9869ECBE40A0C6B12FBAD3155F40A14DBF11DBB4AAC975B3831487'
+$checksum64 = '3ECBB52A939301544B293A05BDD39F76C3329604221F6660824F8150D0BE493A'
+$packageName = $env:ChocolateyPackageName
+
+$shortcutsPath = [Environment]::GetFolderPath("Programs")
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = "$Env:LOCALAPPDATA\"
+  url            = $url
+  url64bit       = $url64
+
+  softwareName   = 'traffic-monitor*'
+
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath (Join-Path -Path $shortcutsPath -ChildPath 'Traffic Monitor.lnk') -targetPath (Join-Path -Path "$Env:LOCALAPPDATA" -ChildPath 'TrafficMonitor\TrafficMonitor.exe')
---
+++ traffic-monitor/update.ps1
@@ -0,0 +1,28 @@
+$releases = 'https://github.com/zhongyang219/TrafficMonitor/releases'
+
+function global:au_GetLatest{
+	$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	
+    $regex = 'x86.7z$'
+    $regex64 = 'x64.7z$'
+    $url = -Join('https://github.com', ($download_page.links | ? href -match $regex | select -First 1 -expand href))
+    $url64 = -Join('https://github.com', ($download_page.links | ? href -match $regex64 | select -First 1 -expand href))
+	
+	$url -match 'TrafficMonitor_V([\d.]+)_' | Out-Null
+    $version = $matches[1]
+	
+    return @{ Version = $version; URL = $url; URL64 = $url64 }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update
