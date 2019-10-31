3c3a11b2600661cd3a723eb261ef767e2783322c
Greenet <lt.tt@outlook.com>
2019-10-31 15:35:09 +0800

    dismplusplus: update to version 10.1.1000.100

---
+++ dismplusplus/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+#$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://cdn.chuyu.me/Dism++10.1.1000.100_2d2bf466baca088c4b35248f5a7316f4e00cac0b.zip'
+$checksum = '3C5887911ABB2A3C47AC7F2D8B9E375583D1742BEAC57EBC3C64E6315218B6A5'
+
+$shortcutsPath = [Environment]::GetFolderPath("Programs")
+$title = $env:ChocolateyPackageTitle
+$architecture = (Get-WmiObject Win32_Processor).AddressWidth
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = "$Env:LOCALAPPDATA\$title\"
+  url            = $url
+
+  softwareName   = 'dismplusplus*'
+
+  checksum       = $checksum
+  checksumType   = 'sha256'
+
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath (Join-Path -Path $shortcutsPath -ChildPath 'Dism++.lnk') -targetPath (Join-Path -Path $Env:LOCALAPPDATA -ChildPath "$title\dism++x$architecture.exe")
---
+++ dismplusplus/update.ps1
@@ -0,0 +1,24 @@
+$releases = 'https://www.chuyu.me/zh-Hans/'
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing #1
+	
+    $regex = ".zip$"
+    $url = $download_page.links | Where-Object href -Match $regex | Select-Object -First 1 -expand href
+
+    $url -match 'Dism\+\+([\d.]+)_'
+    $version = $Matches[1]
+    
+    return @{ Version = $version; URL = $url }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+        }
+    }
+}
+
+update
