314c69bea61b76235cb1e981937ea5e5d3acefad
Greenet <lt.tt@outlook.com>
2019-10-31 15:42:39 +0800

    psutils: update to version 2018.8.5

---
+++ psutils/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/lukesampson/psutils/archive/master.zip'
+$checksum = '4C8AD14D2C137FBEEDCEDC2D0DE4027DCAE2E39D0E80A0709EA9C09717BE093D'
+$packageName = $env:ChocolateyPackageName
+$binPath = "$env:ChocolateyInstall\bin\"
+$utils = @('getopt', 'gitignore', 'ln', 'runat', 'say', 'shasum', 'shim', 'sudo', 'time', 'touch', 'vimtutor')
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  url            = $url
+
+  softwareName   = 'psutils*'
+
+  checksum       = $checksum
+  checksumType   = 'sha256'
+
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Move-Item "$toolsDir\psutils-master\*" $toolsDir
+Remove-Item "$toolsDir\psutils-master"
+
+foreach ($util in $utils) {
+  # Install-BinFile -Name $util -Path "$toolsDir\$util.ps1"
+  Copy-Item -Path "$toolsDir\$util.ps1" -Destination $binPath
+}
---
+++ psutils/tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$binPath = "$env:ChocolateyInstall\bin\"
+$utils = @('getopt', 'gitignore', 'ln', 'runat', 'say', 'shasum', 'shim', 'sudo', 'time', 'touch', 'vimtutor')
+
+foreach ($util in $utils) {
+    # Uninstall-BinFile -Name $util -Path "$toolsDir\$util.ps1"
+    Remove-Item -Path "$binPath\$util.ps1" -Force -ErrorAction SilentlyContinue
+}
