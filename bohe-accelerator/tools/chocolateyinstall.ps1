0cdde5249d2db624f1d4637ec439ff5ee624d039
Greenet <lt.tt@outlook.com>
2019-10-31 15:30:49 +0800

    bohe-accelerator: update to version 1.0.3.4

---
+++ bohe-accelerator/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$url        = 'https://hkupdate.bohe.com/soft/win/1.0.3.4/BOHESetup.1.0.3.4.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = '薄荷加速器'
+
+  checksum      = '8b1f650ba8075d88c1de5770eb8a6a85b0073f3584893cd79b74f325579d9908'
+  checksumType  = 'sha256'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ bohe-accelerator/update.ps1
@@ -0,0 +1,23 @@
+$releases = 'https://cn.bohe.com/config.json'
+
+function global:au_GetLatest{
+	$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing #1
+	
+    $download_page -match 'BOHESetup.([\d.]+).exe'
+    $version = $matches[1]
+    
+    $url = "https://hkupdate.bohe.com/soft/win/$version/BOHESetup.$version.exe"
+	
+    return @{ Version = $version; URL = $url }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+        }
+    }
+}
+
+update
