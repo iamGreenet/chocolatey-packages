3a74633f7e85cfae8809fc8b9a1e0db4eafe434c
Greenet <lt.tt@outlook.com>
2019-10-31 15:36:44 +0800

    netch: update to version 1.3.4

---
+++ netch/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop';
+#$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/NetchX/Netch/releases/download/1.3.4/Netch.1.3.4.x86.7z'
+$checksum = ''
+$url64 = 'https://github.com/NetchX/Netch/releases/download/1.3.4/Netch.1.3.4.x64.7z'
+$checksum64 = '7930376b5e18b609798ee8ab5bccdbe116192ff8530505ebe31a50d2d990560b'
+$title = $env:ChocolateyPackageTitle
+
+$localAppData = $Env:LOCALAPPDATA
+$shortcutsPath = [Environment]::GetFolderPath("Programs")
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = "$localAppData\$title\"
+  url            = $url
+  url64          = $url64
+
+  softwareName   = 'netch*'
+
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath (Join-Path -Path $shortcutsPath -ChildPath 'Netch.lnk') -Target (Join-Path -Path $localAppData -ChildPath "$title\Netch.exe")
---
+++ netch/tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$shortcutsPath = [Environment]::GetFolderPath("Programs")
+$localAppData = $Env:LOCALAPPDATA
+$title = $env:ChocolateyPackageTitle
+$installPath = Join-Path -Path "$localAppData" -ChildPath "$title"
+
+Remove-Item -Path (Join-Path -Path $shortcutsPath -ChildPath 'Netch.lnk') -ErrorAction SilentlyContinue
+if (Test-Path "$installPath") { Remove-Item -Path "$installPath" -Recurse -Force -ErrorAction SilentlyContinue }
