a4290c37e5c518f50d51c9593b4604e1be2b5e10
Greenet <lt.tt@outlook.com>
2019-11-04 12:16:06 +0800

    office-tool: update update.ps1

--- office-tool/update.ps1
+++ office-tool/update.ps1
@@ -3,11 +3,11 @@ $releases = 'https://github.com/YerongAI/Office-Tool/releases'
 function global:au_GetLatest{
 	$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing #1
 	
-    $regex = 'OTP.zip$'
+    $regex = '.zip$'
     $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
     $url = -Join('https://github.com', $url)
-    
-	$download_page.links | ? href -match 'YerongAI/Office-Tool/releases/tag/([\d.]+)' | select -First 1 -expand href
+
+	$url -match 'releases/download/([\d.]+)/'
     $version = $matches[1]
 	
     return @{ Version = $version; URL = $url }
@@ -17,7 +17,7 @@ function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
             "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
