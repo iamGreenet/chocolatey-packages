6152994fdec99625a04e85df27d067bf26014762
Greenim <lt.tt@outlook.com>
2019-11-08 13:40:47 +0800

    mailspring: update to version 1.7.2

---
+++ mailspring/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$url = 'https://github.com/Foundry376/Mailspring/releases/download/1.7.2/MailspringSetup.exe'
+$checksum = 'c9d64ab23a4f368c9b9645c3ed6e4af0642b31af95e21a6f172ed84e48dad28a'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = $url
+
+  softwareName   = 'Mailspring*'
+
+  checksum       = $checksum
+  checksumType   = 'sha256'
+
+  silentArgs     = '-s'
+}
+
+Install-ChocolateyPackage @packageArgs
