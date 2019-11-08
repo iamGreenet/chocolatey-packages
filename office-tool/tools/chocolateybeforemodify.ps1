454fc64ee95f251f59679d19a60687615e239f2e
Greenim <lt.tt@outlook.com>
2019-11-08 16:10:12 +0800

    office-tool: fix invalid shortcut and add beforemodify.ps1

---
+++ office-tool/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,1 @@
+Get-Process "Office Tool Plus" | Stop-Process -Force
--- office-tool/tools/chocolateyinstall.ps1
+++ office-tool/tools/chocolateyinstall.ps1
@@ -4,11 +4,10 @@ $url = 'https://github.com/YerongAI/Office-Tool/releases/download/7.2.0.0/Office
 $checksum = '3d5c02069f7736aadc6cdd9bd3e6715f2ec1fd7cd360e4ae1c2f83fb91e62d4d'
 
 $shortcutsPath = [Environment]::GetFolderPath("Programs")
-$title = $env:ChocolateyPackageTitle
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  unzipLocation  = "$Env:LOCALAPPDATA\$title"
+  unzipLocation  = "$Env:LOCALAPPDATA"
   url            = $url
 
   softwareName   = 'otp*'
@@ -21,4 +20,4 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
-Install-ChocolateyShortcut -shortcutFilePath (Join-Path -Path $shortcutsPath -ChildPath 'Office Tool Plus.lnk') -targetPath (Join-Path -Path $Env:LOCALAPPDATA -ChildPath "$title\Office Tool Plus.exe")
+Install-ChocolateyShortcut -shortcutFilePath (Join-Path -Path $shortcutsPath -ChildPath 'Office Tool Plus.lnk') -targetPath (Join-Path -Path $Env:LOCALAPPDATA -ChildPath "Office Tool Plus.exe")
