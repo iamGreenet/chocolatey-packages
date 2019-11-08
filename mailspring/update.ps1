61b3b9f54b53fb471f31bde06d8c06b9606cdfb9
Greenim <lt.tt@outlook.com>
2019-11-08 13:44:06 +0800

    mailspring: add iconurl and update.ps1

---
+++ mailspring/update.ps1
@@ -0,0 +1,25 @@
+$releases = 'https://github.com/Foundry376/Mailspring/releases'
+
+function global:au_GetLatest{
+	$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing #1
+	
+    $regex = '.exe$'
+    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+	
+	$url = -Join('https://github.com', $url)
+	$url -match '/releases/download/([\d.]+)/' | Out-Null
+    $version = $matches[1]
+	
+    return @{ Version = $version; URL = $url }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update
