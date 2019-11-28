cb70c650be89d77bac0e8919bcbcc281271c2b41
Greenim <lt.tt@outlook.com>
2019-11-28 08:24:41 +0800

    clash-for-windows: Update to version 0.8.4

--- clash-for-windows/tools/chocolateyinstall.ps1
+++ clash-for-windows/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 #$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/Fndroid/clash_for_windows_pkg/releases/download/0.8.3/Clash.for.Windows.Setup.0.8.3.exe'
-$checksum = '26d219c7ecbb5a38e13450bfd15b9551435495976c1d8d6f11b07c4620384944'
+$url = 'https://github.com/Fndroid/clash_for_windows_pkg/releases/download/0.8.4/Clash.for.Windows.Setup.0.8.4.exe'
+$checksum = '2db943a9b768b6e3daa62cc930420489a4efb86ebc91f07afa553baac290c471'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
