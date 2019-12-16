909edb3ebf568b2fe41af2795150f6f60b5d521c
Yato <imYato@outlook.com>
2019-12-16 11:23:50 +0800

    navicat: Update to version 15.0

---
+++ navicat/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,1 @@
+﻿Get-Process Navicat -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
---
+++ navicat/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$url = 'http://download3.navicat.com/download/navicat150_premium_en_x64.exe'
+
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+  fileType     = 'exe'
+  url          = $url
+
+  softwareName = 'PremiumSoft Navicat Premium*'
+
+  checksum     = 'AD30AF49A6B01A69C6535ECA8BCCF2B7DBFB2925422B499815C0DBA62B545E17'
+  checksumType = 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
