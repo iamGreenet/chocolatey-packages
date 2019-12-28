036327ec4e75faab3da5564c4c9816c5da4ce7a1
Yato <imYato@outlook.com>
2019-12-28 14:11:30 +0800

    clash-for-windows: Update to version 0.8.7

--- clash-for-windows/tools/chocolateyinstall.ps1
+++ clash-for-windows/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $url = 'https://github.com/Fndroid/clash_for_windows_pkg/releases/download/0.8.7/Clash.for.Windows.Setup.0.8.7.exe'
-$checksum = ''
+$checksum = '5710a91d75d6d93ab2c512069d0988ace63c947cb159f5f0d19e8d5bab984a9e'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
