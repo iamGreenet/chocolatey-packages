03985d2f66c727bde8a083d49d02a00e5c84df48
Yato <yt@yato.in>
2020-01-08 15:50:40 +0800

    netch: Update to version 1.3.6-pre

--- netch/tools/chocolateyinstall.ps1
+++ netch/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 #$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/NetchX/Netch/releases/download/1.3.5/Netch.1.3.5.x86.7z'
-$checksum = 'f6c59e368ef36d0092f3caf6e18f56fcc8a90b9e9397909e915919355a1c92cd'
-$url64 = 'https://github.com/NetchX/Netch/releases/download/1.3.5/Netch.1.3.5.x64.7z'
-$checksum64 = '03bf6947b131e5ad88663af249351460c0642ee632c53f71635881c9c5a6f4a5'
+$url = 'https://github.com/NetchX/Netch/releases/download/1.3.6PrVer/Netch.1.3.6.x86.7z'
+$checksum = '48469aa3c870f39cea710453184a40950552c7ad15aee85415f9cdd6cbe98853'
+$url64 = 'https://github.com/NetchX/Netch/releases/download/1.3.6PrVer/Netch.1.3.6.x64.7z'
+$checksum64 = '22a52b2375325bccae1a4a28bbf8e05a5fc62d7a50d16684f337eb15daed9225'
 $title = $env:ChocolateyPackageTitle
 
 $localAppData = $Env:LOCALAPPDATA
