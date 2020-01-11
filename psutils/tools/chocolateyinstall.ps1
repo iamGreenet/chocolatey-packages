54d3d8f429d9b2e868b603a42f018efb33a50c3e
Yato <yt@yato.in>
2020-01-11 14:00:11 +0800

    psutils: Update script

--- psutils/tools/chocolateyinstall.ps1
+++ psutils/tools/chocolateyinstall.ps1
@@ -2,12 +2,9 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://github.com/lukesampson/psutils/archive/master.zip'
 $checksum = '4C8AD14D2C137FBEEDCEDC2D0DE4027DCAE2E39D0E80A0709EA9C09717BE093D'
-$packageName = $env:ChocolateyPackageName
-$binPath = "$env:ChocolateyInstall\bin\"
-$utils = @('getopt', 'gitignore', 'ln', 'runat', 'say', 'shasum', 'shim', 'sudo', 'time', 'touch', 'vimtutor')
 
 $packageArgs = @{
-  packageName    = $packageName
+  packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   url            = $url
 
@@ -21,10 +18,5 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
-Move-Item "$toolsDir\psutils-master\*" $toolsDir
-Remove-Item "$toolsDir\psutils-master"
-
-foreach ($util in $utils) {
-  # Install-BinFile -Name $util -Path "$toolsDir\$util.ps1"
-  Copy-Item -Path "$toolsDir\$util.ps1" -Destination $binPath
-}
+Remove-Item -Recurse -ErrorAction SilentlyContinue -Exclude "*.ps1" "$toolsDir\psutils-master\*"
+Install-ChocolateyPath "$toolsDir\psutils-master\"
--- psutils/tools/chocolateyuninstall.ps1
+++ psutils/tools/chocolateyuninstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pathToRemove = "$toolsDir\psutils-master\"
 
-$binPath = "$env:ChocolateyInstall\bin\"
-$utils = @('getopt', 'gitignore', 'ln', 'runat', 'say', 'shasum', 'shim', 'sudo', 'time', 'touch', 'vimtutor')
-
-foreach ($util in $utils) {
-    # Uninstall-BinFile -Name $util -Path "$toolsDir\$util.ps1"
-    Remove-Item -Path "$binPath\$util.ps1" -Force -ErrorAction SilentlyContinue
+$envPath = $env:PATH
+if ($envPath.Contains($pathToRemove)) {
+  $envPath = $envPath -replace [regex]::Escape($pathToRemove + ';'),'' -replace ';;',';'
+  Set-EnvironmentVariable -Name 'Path' -Value $envPath -Scope "User"
 }
