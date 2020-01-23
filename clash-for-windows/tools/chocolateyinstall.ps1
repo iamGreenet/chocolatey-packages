756fdff8b40ab4b4fd776ba17b5d2b5cfd1e030e
Yato <yt@yato.in>
2020-01-23 17:05:05 +0800

    clash-for-windows: Update to version 0.8.10

--- clash-for-windows/tools/chocolateyinstall.ps1
+++ clash-for-windows/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/Fndroid/clash_for_windows_pkg/releases/download/0.8.9/Clash.for.Windows.Setup.0.8.9.exe'
-$checksum = 'b53400d3aa0ca49efdea87b95b9cafa91ec23008efbd882d03770cdaecb6ae91'
+$url = 'https://github.com/Fndroid/clash_for_windows_pkg/releases/download/0.8.10/Clash.for.Windows.Setup.0.8.10.exe'
+$checksum = '79d31258c8a35d3327059272f5b9da5a88ccdd33d9b2264043e7b4adb985bcee'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
