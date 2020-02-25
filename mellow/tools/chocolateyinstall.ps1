bb87f2e745a9bed87395e5bafe7beecebe46745a
Yato <yt@yato.in>
2020-02-25 09:56:48 +0800

    mellow: Update to version 0.1.12

--- mellow/tools/chocolateyinstall.ps1
+++ mellow/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/mellow-io/mellow/releases/download/v0.1.11/Mellow.Setup.0.1.11.exe'
-$checksum = '842fd716f7cf252a9d86f381bf8f27de19f7f1739fd2d583c17eb709d323f546'
+$url = 'https://github.com/mellow-io/mellow/releases/download/v0.1.12/Mellow.Setup.0.1.12.exe'
+$checksum = '58f20447ad628c39891e5557029f58a997caff657e0624f42bd631a6d1146ece'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
