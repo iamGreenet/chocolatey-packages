4728a5b66c096cd84e7657ac57974147fd3de988
Yato <lt.tt@outlook.com>
2020-03-05 18:24:04 +0800

    netch: Update to version 2020.3.5-pre

--- netch/tools/chocolateyinstall.ps1
+++ netch/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 #$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/NetchX/Netch/releases/download/2020%2F2%2F18/Netch.1.3.9Pr.x86.zip'
-$checksum = '85f006047c5cdc1afc74b6130f0de6488db6f4fefa1f5feaa26ab86edc1745a2'
-$url64 = 'https://github.com/NetchX/Netch/releases/download/2020%2F2%2F18/Netch.1.3.9Pr.x64.zip'
-$checksum64 = '8ad3e27289d1f97227348c411ad6bf330a2ad80cc73b1ae4bf5906e883304909'
+$url = 'https://github.com/NetchX/Netch/releases/download/2020%2F3%2F5/Netch.Prx86.2020_3_5.7z'
+$checksum = 'F5556395772AC6C1472ED9D84AE9018B668A5E1D42F4F6837B67F331257D4437'
+$url64 = 'https://github.com/NetchX/Netch/releases/download/2020%2F3%2F5/Netch.Prx64.2020_3_5.7z'
+$checksum64 = '371D7A54A226CE93B93F69D6654479662D99DEC4C884453FF1ABBF2F947763E6'
 $title = $env:ChocolateyPackageTitle
 
 $localAppData = $Env:LOCALAPPDATA
