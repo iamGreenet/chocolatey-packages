415d1217317b07cdfef81607d6e1e7a9478059fa
Yato <lt.tt@outlook.com>
2020-03-05 18:12:26 +0800

    mailspring: Update to version 1.7.4

--- mailspring/tools/chocolateyinstall.ps1
+++ mailspring/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
-$url = 'https://github.com/Foundry376/Mailspring/releases/download/1.7.2/MailspringSetup.exe'
-$checksum = 'c9d64ab23a4f368c9b9645c3ed6e4af0642b31af95e21a6f172ed84e48dad28a'
+$url = 'https://github.com/Foundry376/Mailspring/releases/download/1.7.4/MailspringSetup.exe'
+$checksum = '1e16e6f4f33c25a99e0f78cbec85db0c5f9e91f4b14800acb93223733a7020b6'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
