ce1da1936ff259b59dc9d2f3eacaca249129521f
Yato <i@kix.li>
2020-04-17 11:25:06 +0800

    clash-for-windows: Update to version 0.9.6

--- clash-for-windows/tools/chocolateyinstall.ps1
+++ clash-for-windows/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/Fndroid/clash_for_windows_pkg/releases/download/0.9.5/Clash.for.Windows.Setup.0.9.5.exe'
-$checksum = 'fe4885310634db05ca5b62429764e60046f6d7197ff4eb1d46d907ac732d9d1c'
+$url = 'https://github.com/Fndroid/clash_for_windows_pkg/releases/download/0.9.6/Clash.for.Windows.Setup.0.9.6.exe'
+$checksum = 'fd10a9f8de437e05680e2e7c7ceb1efcd5c957c32ff5c7801715068d2789adf9'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
--- clash-for-windows/update.ps1
+++ clash-for-windows/update.ps1
@@ -1,13 +1,13 @@
 $releases = 'https://github.com/Fndroid/clash_for_windows_pkg/releases'
 
-function global:au_GetLatest{
-	$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	
     $regex = '.exe$'
     $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
 	
-	$url = -Join('https://github.com', $url)
-	$url -match 'Setup\.([\d.]+)\.exe' | Out-Null
+    $url = -Join ('https://github.com', $url)
+    $url -match 'Setup\.([\d.]+)\.exe' | Out-Null
     $version = $matches[1]
 	
     return @{ Version = $version; URL = $url }
@@ -22,4 +22,4 @@ function global:au_SearchReplace {
     }
 }
 
-update
+if ($MyInvocation.InvocationName -ne '.') { update }
