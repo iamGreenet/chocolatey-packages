417e0f6c2d0cae59b670287ac6ec2fca0c8c6c61
Yato <i@kix.li>
2020-04-21 22:59:29 +0800

    shadowsocksr-windows: Update to version 5.1.9

--- shadowsocksr-windows/tools/chocolateyinstall.ps1
+++ shadowsocksr-windows/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/HMBSbige/ShadowsocksR-Windows/releases/download/5.1.8.6/ShadowsocksR-net48-5.1.8.6.zip'
-$checksum = '1e0d144afc1357547154286ad0accf0db7f05dbee840e032f2e88332cc3930fe'
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$fileName = 'ShadowsocksR-net48-5.1.9_x32.zip'
 
 $shortcutsPath = [Environment]::GetFolderPath("Programs")
 $localAppData = $Env:LOCALAPPDATA
@@ -9,12 +9,9 @@ $packageName = $env:ChocolateyPackageName
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = "$localAppData\$packageName"
-  url            = $url
+  file           = Get-Item $toolsPath\$fileName
 
   softwareName   = 'shadowsocksr-windows*'
-
-  checksum       = $checksum
-  checksumType   = 'sha256'
 
   validExitCodes = @(0, 3010, 1641)
 }
--- shadowsocksr-windows/update.ps1
+++ shadowsocksr-windows/update.ps1
@@ -1,24 +1,27 @@
 $releases = 'https://github.com/HMBSbige/ShadowsocksR-Windows/releases'
 
+function global:au_BeforeUpdate() {
+    Get-RemoteFiles -Purge
+}
+
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	
     $regex = 'net48.+zip$'
-    $url = -Join ('https://github.com', ($download_page.links | ? href -match $regex | select -First 1 -expand href))
+    $url = -Join ('https://github.com', ($download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href))
 	
     $url -match 'releases/download/([\d.]+)'
     $version = $Matches[1]
 	
-    return @{ Version = $version; URL = $url }
+    return @{ Version = $version; URL32 = $url }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-            "(^[$]url\s*=\s*)('.*')"        = "`$1'$($Latest.URL)'"
-            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^[$]fileName\s*=\s*)('.*')" = "`$1'$($Latest.FileName32)'"
         }
     }
 }
 
-update
+if ($MyInvocation.InvocationName -ne '.') { update -ChecksumFor none }
