4b660f0152b95c26f7ee7dd37eb0640bffc313fe
Yato <i@kix.li>
2020-04-24 10:09:15 +0800

    tableplus: Update version 3.4.2

--- tableplus/tools/chocolateyinstall.ps1
+++ tableplus/tools/chocolateyinstall.ps1
@@ -1,17 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://s3.amazonaws.com/tableplus-win-builds/122/TablePlusSetup.exe'
-$checksum = '05BB147A189DEB2F86AF70981CB29847E0DC8F32617738273F5F8C28DD2ABAB8'
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$fileName = 'TablePlusSetup.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
+  file           = "$toolsPath\$fileName"
   fileType       = 'exe'
-  url            = $url
-
   softwareName   = 'TablePlus'
-
-  checksum       = $checksum
-  checksumType   = 'sha256'
-
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
