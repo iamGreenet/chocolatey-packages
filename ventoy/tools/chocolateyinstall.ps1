e8b453ed6d30ccbcbf4c1641a3cdbb5cc7ead1a7
Yato <i@kix.li>
2020-04-30 13:00:14 +0800

    ventoy: Update to version 1.0.08-beta2

--- ventoy/tools/chocolateyinstall.ps1
+++ ventoy/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/ventoy/Ventoy/releases/download/v1.0.07/ventoy-1.0.07-windows.zip'
-$checksum = '095B06C21B28EADD69178EFF75CB447F2A61BDC467319D6611771EE44CCC3D39'
+$url = 'https://github.com/ventoy/Ventoy/releases/download/v1.0.08beta2/ventoy-1.0.08b2-windows.zip'
+$checksum = 'e09c465e984cbc10d47e10d6a3f25b4e5379fbf0d48329588102e296ae939f55'
 
 $packageName = $env:ChocolateyPackageName
 $shortcutsPath = Join-Path ([Environment]::GetFolderPath("Programs")) 'Ventoy.lnk'
---
+++ ventoy/update.ps1
@@ -0,0 +1,24 @@
+$releases = 'https://github.com/ventoy/Ventoy/releases'
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
+    $regex = '-windows.zip$'
+    $url = -Join ('https://github.com', ($download_page.links | ? href -match $regex | select -First 1 -expand href))
+
+    $url -match '/download/v?([\w-.]+)/'
+    $version = $matches[1] -replace 'beta','-beta'
+	
+    return @{ Version = $version; URL = $url }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update
