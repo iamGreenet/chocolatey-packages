6e6b2c62df800121e832ede97fdad63729c19185
Yato <i@kix.li>
2020-05-08 13:12:54 +0800

    clash-for-windows: Update to version 0.9.10

--- clash-for-windows/tools/chocolateyinstall.ps1
+++ clash-for-windows/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName64 = 'Clash.for.Windows.Setup.0.9.9.exe'
+$fileName64 = 'Clash.for.Windows.Setup.0.9.10.exe'
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
