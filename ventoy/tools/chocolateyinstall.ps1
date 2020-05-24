224540fffb50e655641d621667d481772a92e9bd
Yato <i@kix.li>
2020-05-24 09:49:25 +0800

    ventoy: Update to version 1.0.11

--- ventoy/tools/chocolateyinstall.ps1
+++ ventoy/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/ventoy/Ventoy/releases/download/v1.0.10/ventoy-1.0.10-windows.zip'
-$checksum = '95b863088b9a234658f323d529c0c52e3edd80fa7799e6cb5d8240fe6db097a6'
+$url = 'https://github.com/ventoy/Ventoy/releases/download/v1.0.11/ventoy-1.0.11-windows.zip'
+$checksum = '13c95b4a5f1c98d54f2c8bde5823144254447f5f9cf39158b126dbcc5a846777'
 
 $packageName = $env:ChocolateyPackageName
 $shortcutsPath = Join-Path ([Environment]::GetFolderPath("Programs")) 'Ventoy.lnk'
