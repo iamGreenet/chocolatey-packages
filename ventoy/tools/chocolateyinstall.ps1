4e2164b5dbc14ff758a3f7e20791c066793335bd
Yato <i@kix.li>
2020-07-10 08:39:49 +0800

    ventoy: Update to v1.0.15

--- ventoy/tools/chocolateyinstall.ps1
+++ ventoy/tools/chocolateyinstall.ps1
@@ -1,10 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/ventoy/Ventoy/releases/download/v1.0.14/ventoy-1.0.14-windows.zip'
-$checksum = '3248b154284335f6a17e5d376e6b85f34795449f81b12e6162d5ae559494f9ff'
+$url = 'https://github.com/ventoy/Ventoy/releases/download/v1.0.15/ventoy-1.0.15-windows.zip'
+$checksum = '030767f1b3b80235066ce912fb626f26b834daebcbff7a576fe0dc45ce63de65'
 
 $packageName = $env:ChocolateyPackageName
 $shortcutsPath = Join-Path ([Environment]::GetFolderPath("Programs")) 'Ventoy.lnk'
 $unzipPath = "$Env:LOCALAPPDATA\$packageName"
+$version = $env:ChocolateyPackageVersion
 
 $packageArgs = @{
   packageName   = $packageName
@@ -17,7 +18,8 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
-Get-ChildItem -Path $unzipPath | Get-ChildItem | Move-Item -Destination $unzipPath
+Copy-Item -Path "$unzipPath/ventoy-$version/*" -Destination $unzipPath -Force -Recurse
+Remove-Item "$unzipPath/ventoy-$version" -Force -Recurse
 
 Install-ChocolateyShortcut -shortcutFilePath $shortcutsPath -Target "$unzipPath\Ventoy2Disk.exe" -WorkingDirectory $unzipPath
 
