735cac9680f49a90c6d4be85c4b9e0d2b06a48d0
Yato <i@kix.li>
2020-07-12 21:03:34 +0800

    picgo: Update to v2.3.0-beta2

--- picgo/tools/chocolateyinstall.ps1
+++ picgo/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/Molunerfinn/PicGo/releases/download/v2.3.0-beta.1/PicGo-Setup-2.3.0-beta.1.exe'
-$checksum = '9dd16104ff95043e181ab3ab0dc14a931f8c5d1ca288366a7430ac62a2b52779'
+$url = 'https://github.com/Molunerfinn/PicGo/releases/download/v2.3.0-beta.2/PicGo-Setup-2.3.0-beta.2.exe'
+$checksum = '3aa8e5725e22a4dc295c9170590594080b0ff7d16caba5dd9b59e81717a4e2ae'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
