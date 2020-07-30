f5b361446301e040ef37237528f7f0e0cf28274c
Yato <i@kix.li>
2020-07-30 19:27:49 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -7,11 +7,12 @@ BOLD_GREEN="\e[1;32m"
 mods=`git status -s | cut -c4- | awk -F'/' '{
 if ($2!="")
 	print $1
-}' | uniq -u`
+}' | sort | uniq`
 
 for mod in $mods; do
 	version=`head "$mod/$mod.nuspec" | grep -oP '(?<=<version>).+(?=<\/version>)'`
-	read -rp "`echo -e \"Update $BOLD_GREEN$mod to v$version$RESET? [Y/n]: \"`" answer
+	read -rp "`echo -e \"Update $BOLD_GREEN$mod to v$version$RESET? [Y/n]: \"`" -n 1 answer
+	echo
 	if [[ $answer == 'n' ]]; then
 		continue
 	fi
