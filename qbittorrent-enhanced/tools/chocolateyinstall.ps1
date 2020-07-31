d10aaa68dfbc0225d8364dfdb94bb7afe122a231
Yato <i@kix.li>
2020-07-31 09:34:51 +0800

    qbittorrent-enhanced: Update to v4.2.5.13

--- qbittorrent-enhanced/tools/chocolateyinstall.ps1
+++ qbittorrent-enhanced/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-4.2.5.12/qbittorrent_4.2.5.12_x64_setup.exe'
-$url64 = 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-4.2.5.12/qbittorrent_4.2.5.12_x64_setup.exe'
-$checksum = '8fbbdb650c425932c242a1716eeb155443eb2a73ad1b2532bbca324a5c000fc8'
-$checksum64 = '8fbbdb650c425932c242a1716eeb155443eb2a73ad1b2532bbca324a5c000fc8'
+$url = 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-4.2.5.13/qbittorrent_4.2.5.13_setup.exe'
+$url64 = 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-4.2.5.13/qbittorrent_4.2.5.13_x64_setup.exe'
+$checksum = 'ce1b946edb72f2d85a0adc298ebea7a853b8c2061a73347f4c36f17930d64e26'
+$checksum64 = '5bf8fec59564f59f0ad989e53be76f23f7417ebd4b592ebb4cdfe3044647a5d1'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
