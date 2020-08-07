3576ff8c039418cf0e992fa4f05252e599debdee
Yato <i@kix.li>
2020-08-07 21:09:14 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -12,8 +12,8 @@ if ($2!="")
 for mod in $mods; do
 	version=`head "$mod/$mod.nuspec" | grep -oP '(?<=<version>).+(?=<\/version>)'`
 	read -rp "`echo -e \"Update $BOLD_GREEN$mod to v$version$RESET? [Y/n]: \"`" -n 1 answer
-	echo
 	if [[ $answer == 'n' ]]; then
+		echo
 		continue
 	fi
 
