4a7b3aece43c5a50bcd5829a24b417f5e1fab7da
Yato <me@oha.moe>
2021-12-31 10:20:10 +0800

    openhashtab: Update to v3.0.1

---
+++ openhashtab/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  file          = (Get-Childitem -Path $toolsDir -Filter "*.exe").fullname
+  softwareName  = 'openhashtab*'
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item "$toolsDir\*.exe" -Force -EA SilentlyContinue | Out-Null
---
+++ openhashtab/update.ps1
@@ -0,0 +1,27 @@
+$releases = 'https://github.com/namazso/openhashtab/releases'
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
+    $regex = '.exe$'
+    $url = -Join ('https://github.com', ($download_page.links | ? href -match $regex | select -First 1 -expand href))
+
+    $url -match '/download/v([\d.]+)/OpenHashTab'
+    $version = $matches[1]
+	
+    return @{ Version = $version; URL32 = $url }
+}
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\VERIFICATION.txt" = @{
+            "(?i)(32-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL32)>"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+  Get-RemoteFiles -Purge -NoSuffix 
+}
+
+update -ChecksumFor none
